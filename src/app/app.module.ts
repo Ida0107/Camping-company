aa97973f349b75a46c77563e118688347e2a3813
somalee gupta <somalee.gupta@gmail.com>
2018-09-06 18:10:21 +0530

    login and register components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDatepickerModule} from 'ngx-bootstrap/datepicker'
-
+import { Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
-import { FooterComponent } from './home-page/footer/footer.component';
+import { FooterComponent } from './common/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AuthModule } from './auth/auth.module';
 
+
+
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch:'full'},
+  {path: 'home', component: HomePageComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomePageComponent,
-    FooterComponent
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    AuthModule,
+    RouterModule.forRoot(routes),
+    
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { Routes, RouterModule} from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '../../../node_modules/@angular/forms';
+import { AuthService } from './shared/auth.service';
+
+const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
+]
+@NgModule({
+  declarations: [
+    
+    RegisterComponent,
+    LoginComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    
+   
+  ],
+  providers: [
+      AuthService
+  ],
+  
+})
+export class AuthModule { }
